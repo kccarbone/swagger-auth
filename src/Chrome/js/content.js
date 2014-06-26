04e01646a40638eb1ffcde0583b4fc4a5272e31f
unknown <kcarbone@117088-ROC-R.marvel.elnk.net>
2014-06-26 13:48:41 -0400

    got initial login working

--- src/Chrome/js/content.js
+++ src/Chrome/js/content.js
@@ -1,11 +1,47 @@
-var contentJS = (function($){
+var content = (function($){
 	
-	var setTokenField = function (val) {
-		$('a').text(val);
+    var fillAuth = function (name, token, userId) {
+        var $tbApiKey = $('#input_apiKey');
+        var $tbUserId = $('#input_RunAs');
+        var $message = $('#sa-message');
+
+        if ($message.length <= 0) {
+            $message = $('<div id="sa-message"></div>').css({
+                position: 'fixed',
+                top: '0',
+                left: '50%',
+                width: '400px',
+                marginLeft: '-200px',
+                padding: '3px',
+                zIndex: '3',
+                fontSize: '13px',
+                textAlign: 'center',
+                color: '#fff',
+                backgroundColor: '#5a5',
+                display: 'none'
+            })
+            .appendTo('body');
+        }
+
+        $tbApiKey
+            .val(token)
+            .css({ backgroundColor: '#ffd' })
+            .on('focus', function () { $(this).css({ backgroundColor: '#fff' }) });
+
+        // Inject the button click event directly into the page so that it uses
+        // the page's own jQuery instance and event handlers
+        var script = document.createElement('script');
+        script.textContent = '$("#explore").trigger("click");';
+        document.head.appendChild(script);
+
+        $message.text('Authorized user: ' + name)
+            .show()
+            .delay(3000)
+            .fadeOut(2000);
 	};
 	
 	return {
-		setTokenField: setTokenField
+	    fillAuth: fillAuth
 	}
 	
 })(jQuery);
--- src/Chrome/js/popup.js
+++ src/Chrome/js/popup.js
@@ -13,16 +13,14 @@ var init = function () {
 	});
 
 	$('#btnUserLogin').on('click', function () {
-        setLoading();
-	});
-	
-	// TODO: wrap this in the browser action click handler to avoid permission warning
-	injectContentJS(function () {
-		
+	    setLoading();
+	    saveLoginInfo();
+	    getOAuthToken();
 	});
 	
 	setActiveIndex(0);
 	loadPrefs();
+	loadLoginInfo();
 };
 
 var setActiveIndex = function (index) {
@@ -31,13 +29,14 @@ var setActiveIndex = function (index) {
 };
 
 var setLoading = function () {
+    clearError();
     $('.loadingBar').show();
-    $('button').prop('disabled', true);
+    $('button, input, checkbox, select').prop('disabled', true);
 };
 
 var clearLoading = function () {
     $('.loadingBar').hide();
-    $('button').prop('disabled', false);
+    $('button, input, checkbox, select').prop('disabled', false);
 };
 
 var loadPrefs = function () {
@@ -54,14 +53,129 @@ var loadPrefs = function () {
 	});
 };
 
-var injectContentJS = function (callback) {
-	chrome.tabs.executeScript(null, { file: "jquery.js" }, function(){
-		chrome.tabs.executeScript(null, { file: "content.js" }, function(){
-			if(callback){
-				callback();
-			}
-		});
-	});
+var saveLoginInfo = function () {
+    var $username = $('#tbUserName');
+    var $password = $('#tbPassword');
+    var $savePassword = $('#cbSavePassword');
+    var $impUsername = $('#tbImpUserName');
+    var $impPassword = $('#tbImpPassword');
+    var $impSavePassword = $('#cbImpSavePassword');
+
+    if ($savePassword.is(':checked')) {
+        chrome.storage.sync.set({
+            savedLogin:
+                {
+                    username: $username.val(),
+                    password: $password.val(),
+                    savePassword: $savePassword.is(':checked')
+                }
+        });
+    }
+    else {
+        chrome.storage.sync.set({ savedLogin: { savePassword: false } });
+    }
+
+    if ($impSavePassword.is(':checked')) {
+        chrome.storage.sync.set({
+            savedImpLogin:
+                {
+                    impUsername: $impUsername.val(),
+                    impPassword: $impPassword.val(),
+                    impSavePassword: $impSavePassword.is(':checked')
+                }
+        });
+    }
+    else {
+        chrome.storage.sync.set({ savedImpLogin: { impSavePassword: false } });
+    }
+};
+
+var loadLoginInfo = function () {
+    var $username = $('#tbUserName');
+    var $password = $('#tbPassword');
+    var $savePassword = $('#cbSavePassword');
+    var $impUsername = $('#tbImpUserName');
+    var $impPassword = $('#tbImpPassword');
+    var $impSavePassword = $('#cbImpSavePassword');
+
+    chrome.storage.sync.get('savedLogin', function (result) {
+        if (result.savedLogin && result.savedLogin.savePassword) {
+            $username.val(result.savedLogin.username);
+            $password.val(result.savedLogin.password);
+            $savePassword.prop('checked', result.savedLogin.savePassword);
+        }
+    });
+
+    chrome.storage.sync.get('savedImpLogin', function (result) {
+        if (result.savedImpLogin && result.savedImpLogin.impSavePassword) {
+            $impUsername.val(result.savedImpLogin.impUsername);
+            $impPassword.val(result.savedImpLogin.impPassword);
+            $impSavePassword.prop('checked', result.savedImpLogin.impSavePassword);
+        }
+    });
+};
+
+var getOAuthToken = function () {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        var tab = tabs[0];
+        var environment;
+        var username = $('#tbUserName').val();
+        var password = $('#tbPassword').val();
+        var userId = '';
+
+        if ($('.content').eq(1).is('.active')) {
+            username = $('#tbImpUserName').val();
+            password = $('#tbImpPassword').val();
+        }
+        
+        if (!tab) { showError('Tab not found'); return; }
+        if (!preferences) { showError('Preferences not found'); return; }
+        if (!preferences.Token_Auth) { showError('Token_Auth not found'); return; }
+        if (!preferences.Token_Auth.grant_type) { showError('grant_type not found'); return; }
+        if (!preferences.Token_Auth.client_id) { showError('client_id not found'); return; }
+        if (!preferences.Token_Auth.client_secret) { showError('client_secret not found'); return; }
+        if (!preferences.Environments) { showError('No environments configured'); return; }
+        if (preferences.Environments.length <= 0) { showError('No environments configured'); return; }
+
+        $.each(preferences.Environments, function () {
+            if (tab.url.indexOf(this.API_url) >= 0) {
+                environment = this;
+            }
+        });
+
+        if (!environment) { showError('Environment not recognized'); return; }
+
+        $.post(environment.IDP_url,
+            {
+                username: username,
+                password: password,
+                grant_type: preferences.Token_Auth.grant_type,
+                client_id: preferences.Token_Auth.client_id,
+                client_secret: preferences.Token_Auth.client_secret
+            }
+        )
+        .done(function (data) {
+            clearLoading();
+
+            chrome.tabs.executeScript(tab.id, { file: "js/jquery.js" }, function () {
+                chrome.tabs.executeScript(tab.id, { file: "js/content.js" }, function () {
+                    chrome.tabs.executeScript(tab.id, { code: "content.fillAuth('" + username + "', '" + data.access_token + "', '" + userId + "');" });
+                });
+            });
+        })
+        .fail(function () {
+            showError('Authenication failed');
+        });
+    });
+};
+
+var showError = function (msg) {
+    clearLoading();
+    $('.error-message').html(msg).show();
+};
+
+var clearError = function (msg) {
+    $('.error-message').hide();
 };
 
 $(function () {
